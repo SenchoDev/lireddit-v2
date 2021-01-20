33f8399ec5954f639475621c846c5972b678b22e
Senadin <beganovicsenadin24@gmail.com>
2021-01-20 14:25:22 +0100

    implemented error handling and added login and register functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+
 import 'reflect-metadata';
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
@@ -7,6 +8,7 @@ import express from 'express';
 import { buildSchema } from 'type-graphql';
 import { ApolloServer } from "apollo-server-express";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from './resolvers/user';
 const main = async () => {
   
   const orm = await MikroORM.init(microConfig)
@@ -16,7 +18,7 @@ const main = async () => {
 
   const appolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [PostResolver],
+      resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em })
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,15 @@
 import { User } from "./../entities/User";
-import { Arg, Ctx, Resolver, Mutation, InputType, Field } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Resolver,
+  Mutation,
+  InputType,
+  Field,
+  ObjectType,
+} from "type-graphql";
 import { MyContext } from "src/types";
+import argon2 from "argon2";
 
 @InputType()
 class UsernamePasswordInput {
@@ -10,15 +19,96 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [Error], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => String)
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
-    const user = em.create(User, { username: options.username });
-    await em.persistAndFlush(user);
-    return "bye";
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater than 2",
+          },
+        ],
+      };
+    }
+
+    if (options.password.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "length must be greater than 2",
+          },
+        ],
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    try{
+      await em.persistAndFlush(user);
+    } catch(err){
+      if(err.code === '23505' || err.detail.includes("already exists")){
+        return {
+          errors: [
+            {
+              field: "user",
+              message: "username already taken",
+            },
+          ],
+        };
+      }
+    }
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+
+    if (!user) {
+      return {
+        errors: [{ field: "username", message: "that username doesn't exist" }],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid) {
+      return {
+        errors: [{ field: "password", message: "incorrect password" }],
+      };
+    }
+    return {
+      user,
+    };
   }
 }
