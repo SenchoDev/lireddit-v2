270d63f713180316c4381c5bc4c1bbd7d9a9f4bf
Senadin <beganovicsenadin24@gmail.com>
2021-01-20 10:52:47 +0100

    finished setting up graphql

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+//import { Post } from "./entities/Post";
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+import { ApolloServer } from "apollo-server-express";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
+  
   const orm = await MikroORM.init(microConfig)
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, {title: 'my first post'});
+  const app = express();
 
-  await orm.em.persistAndFlush(post);
-  // await orm.em.nativeInsert(Post, {title: 'my first post 2'});
+  const appolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+
+    })
+  })
+
+  appolloServer.applyMiddleware({ app })
+
+  app.get('/', (req, res) => {
+    res.send('hello')
+  })
+
+  app.listen(4000, () =>{
+    console.log('server started on localhost:400')
+  })
 
 }
 
